db1fe4444c87540c08f104d0a73fd350e6f81c0d
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-09-17 17:48:35 +0300

    connecting to backend in progress

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,26 +1,47 @@
-import { Component, inject } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators,} from '@angular/forms';
+import { Component, inject, NgModule } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators,} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+
+function passwordRules(control: AbstractControl)
+{
+  const value = control.value || '';
+
+  const hasNumber = /\d/.test(value);
+  const hasSymbol = /[^A-Za-z0-9]/.test(value);
+  const hasUpperCase = /[A-Z]/.test(value);
+
+  if (!hasNumber || !hasSymbol || !hasUpperCase) {
+    return { passwordRules: true };
+  }
+
+  return null;
+}
+
+
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 
 export class register {
+  private httpClient = inject(HttpClient);
   form = new FormGroup({
     email : new FormControl('',{
       validators: [Validators.required, Validators.email,],
     }),
     password : new FormControl('',{
-      validators: [Validators.required, Validators.minLength(6),],
+      validators: [passwordRules,Validators.required, Validators.minLength(6),],
     }),
     userName : new FormControl('',{
       validators: [Validators.required, Validators.minLength(6),Validators.maxLength(15)],
     }),
   });
-  get emailIsInvalid(){
+   get emailIsInvalid(){
     return this.form.controls.email.invalid 
     && this.form.controls.email.touched
     && this.form.controls.email.dirty;
@@ -29,6 +50,11 @@ export class register {
     return this.form.controls.password.invalid 
     && this.form.controls.password.touched
     && this.form.controls.password.dirty;
+  }
+  get userNameIsInvalid(){
+    return this.form.controls.userName.invalid 
+    && this.form.controls.userName.touched
+    && this.form.controls.userName.dirty;
   }
   onSubmit() 
   {
