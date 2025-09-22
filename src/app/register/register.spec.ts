f43ecbcbbbcd9ced54838e681b0b7ae520abafaa
Ahmed Hossam <143847628+AhmedHossam7411@users.noreply.github.com>
2025-09-22 13:14:49 +0300

    Feature 04/register form (#8)
    * add register form
    
    * finalize register form
    
    * connecting to backend in progress
    
    * register working with backend
    
    * lines removed
    
    * reload

---
+++ src/app/register/register.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { register } from './register';
+
+describe('Register', () => {
+  let component: register;
+  let fixture: ComponentFixture<register>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [register]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(register);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/register/register.ts
@@ -0,0 +1,81 @@
+import { Component, inject, NgModule } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators,} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RegisterRequest } from './registerReq.model';
+
+
+function passwordRules(control: AbstractControl)
+{
+  const value = control.value || '';
+
+  const hasNumber = /\d/.test(value);
+  const hasSymbol = /[^A-Za-z0-9]/.test(value);
+  const hasUpperCase = /[A-Z]/.test(value);
+
+  if (!hasNumber || !hasSymbol || !hasUpperCase) {
+    return { passwordRules: true };
+  }
+
+  return null;
+}
+
+
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+
+export class register {
+  private httpClient = inject(HttpClient);
+  private apiUrl='https://localhost:7285';
+
+  form = new FormGroup({
+    email : new FormControl('',{
+      validators: [Validators.required, Validators.email,],
+    }),
+    password : new FormControl('',{
+      validators: [passwordRules,Validators.required, Validators.minLength(6),],
+    }),
+    userName : new FormControl('',{
+      validators: [Validators.required, Validators.minLength(6),Validators.maxLength(15)],
+    }),
+  });
+   get emailIsInvalid(){
+    return this.form.controls.email.invalid 
+    && this.form.controls.email.touched
+    && this.form.controls.email.dirty;
+  }
+   get passwordIsInvalid(){
+    return this.form.controls.password.invalid 
+    && this.form.controls.password.touched
+    && this.form.controls.password.dirty;
+  }
+  get userNameIsInvalid(){
+    return this.form.controls.userName.invalid 
+    && this.form.controls.userName.touched
+    && this.form.controls.userName.dirty;
+  }
+  onSubmit() {
+     if (this.form.invalid) {
+    this.form.markAllAsTouched(); 
+    return;
+  }
+  const formValue: RegisterRequest = this.form.value as RegisterRequest;
+
+  this.register(formValue).subscribe({
+    next: (res) => {
+      console.log('Registered successfully:', res);
+      window.location.reload(); 
+    },
+    error: (err) => console.error('Registration failed:', err)
+  });
+}
+ register(data: RegisterRequest): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/api/Auth/register`, data);
+  }
+}
