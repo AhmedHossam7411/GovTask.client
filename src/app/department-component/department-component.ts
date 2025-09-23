b622ed3aaba95a07e035e1d4080eee9dcb79225c
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-09-23 14:06:50 +0300

    department branch

---
+++ src/app/department-component/department-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-component',
+  imports: [],
+  templateUrl: './department-component.html',
+  styleUrl: './department-component.css'
+})
+export class DepartmentComponent {
+
+}
