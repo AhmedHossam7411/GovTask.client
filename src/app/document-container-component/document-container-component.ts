eee6f24cf650537c96c3f44d8da105d39688afdc
Ahmed Hossam <ahmedabdelkhalek.7411@gmail.com>
2026-03-04 01:13:13 +0200

    adding needed files for document module , still empty

---
+++ src/app/document-container-component/document-container-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-container-component',
+  imports: [],
+  templateUrl: './document-container-component.html',
+  styleUrl: './document-container-component.css'
+})
+export class DocumentContainerComponent {
+
+}
