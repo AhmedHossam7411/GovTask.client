2fa336b8cbf33c50d8d0eab10cd4fac6098c8ffc
Ahmed Hossam <ahmedabdelkhalek.7411@gmail.com>
2026-03-16 17:12:02 +0200

    edit form fix in departments

--- src/app/department-component/edit-form/edit-form.ts
+++ src/app/department-component/edit-form/edit-form.ts
@@ -10,45 +10,34 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   templateUrl: './edit-form.html',
   styleUrl: './edit-form.css'
 })
-export class EditForm implements OnInit {
+export class EditForm {
   private departmentService = inject(DepartmentService);
   private dialogRef = inject(MatDialogRef);
   data = inject(MAT_DIALOG_DATA) as DepartmentDto;
   
-  ngOnInit() {
-    console.log(this.data);
-    this.form.controls.name.setValue(this.data.name);
-  }
-
 form = new FormGroup({
    name : new FormControl('',{
       validators: [Validators.required,Validators.minLength(6) ],
     }),
 });
-
-updateDepartment(id: number , departmentDto:DepartmentDto)
-  {
-    if (this.form.valid && this.data) {
-      console.log(this.data);
-    const updated: DepartmentDto = {
-      id: this.data.id,
-      name: this.form.value.name ?? this.data.name
-         
-    };
-    console.log("logger",this.data.name,this.data.id);
-    this.data.name = updated.name;
-    this.departmentService.putDepartment(id,departmentDto).subscribe({
-      next: (response) => {
-        console.log(this.data.name);
-        console.log(this.data.name);
-
-          this.closeDialog(); 
-          
-        },
-        error: (err) => console.error(err),
-    })
+ 
+  updateDepartment(){
+    if(this.form.valid)    {
+      const updatedDepartment: DepartmentDto = {
+        id: this.data.id,
+        name: this.form.value.name || this.data.name
   }
-}
+      this.departmentService.putDepartment
+      (this.data.id,updatedDepartment).subscribe({
+        next: (res) => {
+          this.dialogRef.close(res);
+        },
+        error: (err) => {
+          console.error('Failed to update department', err);
+        }
+      });
+    }
+  }
   get nameIsInvalid(){
     return this.form.controls.name.invalid 
     && this.form.controls.name.touched
